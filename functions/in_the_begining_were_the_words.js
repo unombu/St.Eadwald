acd76e750af229c816eec19063a85f5d38761853
unombu <belen.ungari@gmail.com>
2025-06-08 02:22:20 -0300

    una teca

--- functions/in_the_begining_were_the_words.js
+++ functions/in_the_begining_were_the_words.js
@@ -1,8 +1,19 @@
 exports.handler = async function(event, context) {
-    const WORDS = [
-        "LINDO", "CUTIE", "TALOS", "SONIC", "ALIEN", "BESOS", "CALVO", "VERDE", "JUEGO",
-        "PLUTO", "MOMMY", "PLEASE", "SILLA"
-    ];
+    const WORDS = {
+        0 : {word : "LINDO"}, 
+        1 : {word : "CUTIE"},
+        587 : {word : "TALOS"},
+        71 : {word : "SONIC"},
+        27 : {word : "ALIEN"},
+        2 : {word : "BESOS"},
+        3 : {word : "CALVO"},
+        4 : {word : "VERDE"},
+        5 : {word : "JUEGO"},
+        9 : {word : "PLUTO"},
+        10 : {word : "MOMMY"},
+        11 : {word : "DADDY"},
+        12 : {word : "SILLA"}
+    };
 
     let index = event.queryStringParameters.i;
 
@@ -13,9 +24,9 @@ exports.handler = async function(event, context) {
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*", // ¡Importante para permitir peticiones desde tu frontend!
-            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Allow-Methods": "GET",
             "Access-Control-Allow-Headers": "Content-Type"
         },
-        body: JSON.stringify({ word: word , i : index }) // Envía la palabra como un objeto JSON
+        body: JSON.stringify({ word: word.word}) // Envía la palabra como un objeto JSON
     };
 }
